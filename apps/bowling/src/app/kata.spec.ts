0349af4f95e709d4ce6f243c6c9d9ade32a0a76b
linyc0817 <f0922571727@gmail.com>
2022-05-04 21:28:48 +0800

    Finish testBasics (greenlight)

--- apps/bowling/src/app/kata.spec.ts
+++ apps/bowling/src/app/kata.spec.ts
@@ -11,6 +11,11 @@ describe('Kata', () => {
     //   game.roll(0);
     // }
     expect(kata.price([])).toBe(0);
+    expect(kata.price([1])).toBe(8);
+    // expect(kata.price([2])).toBe(8);
+    // expect(kata.price([3])).toBe(8);
+    // expect(kata.price([4])).toBe(8);
+    expect(kata.price([1,1,1])).toBe(8*3);
   });
   
 });
--- apps/bowling/src/app/kata.ts
+++ apps/bowling/src/app/kata.ts
@@ -1,9 +1,13 @@
 export class Kata {
   price(arg0: number[]): any {
     // throw new Error('Method not implemented.');
-    console.log(arg0);
+    let bookNum = 0;
     if(!arg0.length)
-        return 1;
-    return 0;
+        return 0;
+    // console.log(arg0[3]);
+    for (let i = 0; i < arg0.length; i++) {
+        bookNum += arg0[i];
+      }
+    return bookNum*8;
   }
 }
