dcdc7885895d997d776f667b1c28ef31d289adbf
linyc0817 <f0922571727@gmail.com>
2022-05-12 13:46:24 +0800

    Modify testBasic to correct answer

--- apps/bowling/src/app/kata.spec.ts
+++ apps/bowling/src/app/kata.spec.ts
@@ -1,3 +1,4 @@
+import { IterableDiffers } from '@angular/core';
 import { Kata } from './kata';
 
 describe('Kata', () => {
@@ -7,15 +8,19 @@ describe('Kata', () => {
 
   test('testBasics', () => {
     const kata = new Kata();
-    // for (let i = 0; i < 20; i++) {
-    //   game.roll(0);
-    // }
     expect(kata.price([])).toBe(0);
     expect(kata.price([1])).toBe(8);
-    // expect(kata.price([2])).toBe(8);
-    // expect(kata.price([3])).toBe(8);
-    // expect(kata.price([4])).toBe(8);
+    expect(kata.price([2])).toBe(16);
+    expect(kata.price([3])).toBe(24);
+    expect(kata.price([4])).toBe(32);
     expect(kata.price([1,1,1])).toBe(8*3);
   });
+
+  it('test it', ()=>{
+    const kata = new Kata();
+    expect(kata.price([])).toEqual(0);
+    expect(kata.price([1])).toEqual(8);
+  });
+
   
 });
